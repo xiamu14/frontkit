0ea66c979643fd393feec622f15623384fbaef1a
ben <544652471@qq.com>
2020-03-20 10:03:49 +0800

    feat: change some feature.

--- src/event/index.js
+++ src/event/index.js
@@ -3,16 +3,16 @@ const { ipcMain, dialog } = require("electron");
 module.exports = function event() {
   // 选择目录/文件
   ipcMain.on("open-directory-dialog", (event, arg) => {
-    console.log(arg); // prints "ping"
+    // console.log(arg); // prints "ping"
     const directory = dialog.showOpenDialogSync({
         properties: arg
       })
-    console.log('查看一下路径', directory);
+    // console.log('查看一下路径', directory);
     event.reply("selected-directory", directory);
   });
   // 动态导入模块测试
   ipcMain.on("import-dynamic", (event, arg) => {
-    console.log('动态导入模块执行了吗');
+    // console.log('动态导入模块执行了吗');
     const test = require('./dynamic-test.js')
     test();
   })
